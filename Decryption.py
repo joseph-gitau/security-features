7aef1bd525e5075d4d30b14665986f0c712d7d1c
joseph-gitau <crosetsw09@gmail.com>
2024-05-01 13:46:07 +0300

    add decryption

--- Decryption.py
+++ Decryption.py
@@ -5,11 +5,18 @@ from base64 import b64encode, b64decode
 key = utils.random(secret.SecretBox.KEY_SIZE)
 box = secret.SecretBox(key)
 
-# Your sensitive data
-message = b"Sensitive data that needs to be encrypted"
+# sensitive data to be decrypted
+message = b"This is a secret message!"
 
 # Encrypt the message
 encrypted = box.encrypt(message)
 encrypted_text = b64encode(encrypted).decode('utf-8')
 
 print(f"Encrypted: {encrypted_text}")
+
+# Decode the encrypted message
+encrypted = b64decode(encrypted_text)
+
+# Decrypt the message
+decrypted = box.decrypt(encrypted)
+print(f"Decrypted: {decrypted.decode('utf-8')}")
